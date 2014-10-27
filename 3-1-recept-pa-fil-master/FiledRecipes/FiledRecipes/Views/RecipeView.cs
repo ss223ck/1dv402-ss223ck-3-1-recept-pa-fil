5990853411c9fea6bbcaf550722fd76d018bb4f6
ss223ck <ss223ck@student.lnu.se>
2014-10-27 17:31:37 +0100

    Done with the assignment

---
+++ 3-1-recept-pa-fil-master/FiledRecipes/FiledRecipes/Views/RecipeView.cs
@@ -0,0 +1,51 @@
+﻿using FiledRecipes.Domain;
+using FiledRecipes.App.Mvp;
+using FiledRecipes.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FiledRecipes.Views
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class RecipeView : ViewBase, IRecipeView
+    {
+
+        public void Show(IRecipe recipe)
+        {
+            Console.Clear();
+            Header = recipe.Name;
+            ShowHeaderPanel();
+            int instructionValue = 1;
+
+            Console.WriteLine();
+            Console.WriteLine("Ingredienser");
+            Console.WriteLine("============");
+            foreach (Ingredient i in recipe.Ingredients)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Gör så här");
+            Console.WriteLine("==========");
+            foreach (string s in recipe.Instructions)
+            {
+                Console.WriteLine(string.Format("<{0}>", instructionValue));
+                Console.WriteLine(s);
+                instructionValue++;
+            }
+            
+        }
+        public void Show(IEnumerable<IRecipe> recipes)
+        {
+            foreach (Recipe r in recipes)
+            {
+                Show(r);
+                ContinueOnKeyPressed();
+            }
+        }
+    }
+}
